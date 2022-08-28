2a5f15899ec0876b863d04cc701567cea3950f57
Júnior Albuquerque <juioralbuquerque81@gmail.com>
2022-08-27 21:20:05 -0300

    chore: create header component and add global styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <img src="/logo.svg" alt="logo" width="239" height="26" />
+      </Link>
+    </div>
+  );
 }
